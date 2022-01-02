5e4dfae70191400faa8e0cef6ad20451b00317a1
nathan-zaloum <nathan@zaloum.co.nz>
2022-01-03 11:08:15 +1300

    Added styling for new entry form

--- db/migrate/20220101054127_create_journals.rb
+++ db/migrate/20220101054127_create_journals.rb
@@ -5,8 +5,6 @@ class CreateJournals < ActiveRecord::Migration[7.0]
       t.decimal :weight
       t.text :entry
       t.boolean :gym_attendance
-      t.time :gym_duration
-      t.string :gym_type
       t.integer :water
       t.integer :calories
 
