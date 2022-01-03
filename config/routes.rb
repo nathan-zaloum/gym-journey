503b38cf739d8590197b76df18c203998b88acdb
nathan-zaloum <nathan@zaloum.co.nz>
2022-01-04 00:07:58 +1300

    Added Devise and set up basic auth

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  devise_for :users
+  # devise_for :users do
+  #   get "/users/sign_out" => "devise/sessions#destroy"
+  # end
   resources :journals
   root "home#index"
 end
